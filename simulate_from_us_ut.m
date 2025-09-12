91c64e1d07bb6383faa5449ac1a8c3928e7250b9
eddy22 <72153099+edward-nketiah@users.noreply.github.com>
2025-09-12 11:48:15 +0800

    Update simulate_from_us_ut.m

--- simulate_from_us_ut.m
+++ simulate_from_us_ut.m
@@ -2,11 +2,11 @@ function sim_data = simulate_from_us_ut(us_b, ut_b, N_target,Ts)
     %S = [0 20 0 20];
     %T = [0,750];
     %Tmax = max(space_time_data(:,3));
-    N = poissrnd(Ts*8*15*254.3551);
+    N = poissrnd(Ts*20*20*750);
     % Estimate max values for rejection bounds
-    test_pts_x = rand(N,1)*8;
-    test_pts_y = rand(N,1)*15;
-    test_pts_t = rand(N,1)*Tmax;
+    test_pts_x = rand(N,1)*20;
+    test_pts_y = rand(N,1)*20;
+    test_pts_t = rand(N,1)*750;
     
     us_vals = us_b(test_pts_x, test_pts_y);
     ut_vals = ut_b(test_pts_t);
@@ -17,12 +17,13 @@ function sim_data = simulate_from_us_ut(us_b, ut_b, N_target,Ts)
     % Sample loop
     sim_data = [];
     while size(sim_data, 1) < N_target
-        x = rand() * 8;
-        y = rand() * 15;
-        t = rand() * Tmax;
+        x = rand() *20;
+        y = rand() * 20;
+        t = rand() *750;
         
         if rand() < (us_b(x,y)*ut_b(t) / us_max*ut_max)
             sim_data = [sim_data; x, y, t];
         end
     end
+
 end
